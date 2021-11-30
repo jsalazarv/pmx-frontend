26713423dcd2aaeca00e97b91a967248db10e6c4
Juan Salazar <juan.salazar@grupocsi.com>
2021-11-30 16:08:56 -0600

    Assignment Constancy view was added

---
+++ src/lang/es/mfe/proofAssignment.ts
@@ -0,0 +1,43 @@
+export default {
+  list: {
+    title: "Constancia de asignación",
+  },
+  edit: {
+    title: "",
+  },
+  create: {
+    title: "",
+  },
+  delete: {
+    title: "",
+  },
+  attributes: {
+    typeOfEmployee: "Tipo de empleado",
+    names: "Nombres",
+    lastname: "Apellido paterno",
+    surname: "Apellido materno",
+    curp: "CURP",
+    assignmentNumber: "Número de asignación",
+    assignmentNumberStatus: "Estado de número de asignación",
+    birthday: "Fecha de cumpleaños",
+    gender: "Sexo",
+    rfc: "RFC",
+  },
+  labels: {
+    add: "Agregar",
+    export: "Exportar",
+    search: "Buscar",
+    dialogs: {
+      searchNotFound: {
+        message: "",
+      },
+      confirmDelete: {},
+      successDelete: {},
+      errorDelete: {},
+      errorUpdate: {},
+      successUpdate: {},
+      errorCreate: {},
+      successCreate: {},
+    },
+  },
+};
