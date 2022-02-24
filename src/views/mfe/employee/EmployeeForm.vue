13744175816f31f1a0bcb944d4d01b19271837d0
Juan Salazar <juan.salazar@grupocsi.com>
2022-02-24 14:20:20 -0600

    Se agregó plantilla basica de formulario de contacto

--- src/views/mfe/employee/EmployeeForm.vue
+++ src/views/mfe/employee/EmployeeForm.vue
@@ -14,20 +14,54 @@
           :indeterminate="isLoading"
         ></v-progress-linear>
         <ValidationObserver ref="form" v-slot="{ invalid }">
-          <employee-search-form />
-          <employment-information-form />
-          <location-form />
-          <v-container>
-            <v-btn
-              color="success"
-              @click="registerEmployee"
-              :disabled="invalid || isCreating"
-              :loading="isCreating"
-            >
-              {{ $t("employee.labels.generateAssignmentNumber") }}
-              <v-icon right dark>mdi-plus-thick</v-icon>
-            </v-btn>
-          </v-container>
+          <v-stepper v-model="e1">
+            <v-stepper-header>
+              <v-stepper-step :complete="e1 > 1" step="1">
+                Información personal
+              </v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step :complete="e1 > 2" step="2">
+                Name of step 2
+              </v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step :complete="e1 > 3" step="3">
+                Name of step 3
+              </v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step step="4"> Name of step 4 </v-stepper-step>
+            </v-stepper-header>
+
+            <v-stepper-items>
+              <v-stepper-content step="1">
+                <employee-search-form />
+                <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+              <v-stepper-content step="2">
+                <employment-information-form />
+                <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+              <v-stepper-content step="3">
+                <location-form />
+                <v-btn color="primary" @click="e1 = 4"> Continue </v-btn>
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+              <v-stepper-content step="4">
+                <contact-form />
+                <v-btn
+                  color="success"
+                  @click="registerEmployee"
+                  :disabled="invalid || isCreating"
+                  :loading="isCreating"
+                >
+                  {{ $t("employee.labels.generateAssignmentNumber") }}
+                  <v-icon right dark>mdi-plus-thick</v-icon>
+                </v-btn>
+                <v-btn text> Cancel </v-btn>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
         </ValidationObserver>
       </v-card>
     </div>
@@ -43,9 +77,11 @@ import EmploymentInformationForm from "@/views/mfe/employee/components/Employmen
 import LocationForm from "@/views/mfe/employee/components/LocationForm.vue";
 import EmployeeService from "@/services/EmployeeService";
 import { IValidationObserver } from "@/components/types";
+import ContactForm from "@/views/mfe/employee/components/ContactForm.vue";
 
 @Component({
   components: {
+    ContactForm,
     EmployeeSearchForm,
     LocationForm,
     EmploymentInformationForm,
@@ -55,6 +91,7 @@ export default class PeopleCreate extends Vue {
   protected employeeService = new EmployeeService();
 
   public isCreating = false;
+  public e1 = 1;
 
   get isLoading(): boolean {
     // TODO Refactor this form is submitting
---
+++ src/views/mfe/employee/components/ContactForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4"></v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component()
+export default class ContactForm extends Vue {}
+</script>
+
+<style scoped></style>
